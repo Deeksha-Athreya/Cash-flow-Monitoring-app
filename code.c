d924c3d680217d47a2f9a7e3a74241a16eae6c2f
Deeksha <117797100+Deeksha-Athreya@users.noreply.github.com>
2023-11-14 21:09:09 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 struct Person {
     char name[50];
     int* debts;
@@ -11,23 +12,21 @@ void initializePersons(struct Person* persons, char** names, int n) {
     for (int i = 0; i < n; i++) {
         strcpy(persons[i].name, names[i]);
         persons[i].debts = malloc(n * sizeof(int));
+        persons[i].amount = 0;
 
         for (int j = 0; j < n; j++) {
-            if(i!=j){
-            printf("Enter the amount owed by %s to %s: ", persons[i].name, names[j]);
-            scanf("%d", &persons[i].debts[j]);
-            persons[i].amount += persons[i].debts[j];
-        }
-        else{
-            continue;
-        }
+            if (i != j) {
+                printf("Enter the amount owed by %s to %s: ", persons[i].name, names[j]);
+                scanf("%d", &persons[i].debts[j]);
+                persons[i].amount += persons[i].debts[j];
+            }
         }
     }
 }
 
 void settleAmounts(struct Person* persons, char** names, int n) {
-    // Calculate net amount for each person
     int* netAmounts = malloc(n * sizeof(int));
+
     for (int i = 0; i < n; i++) {
         netAmounts[i] = 0;
         for (int j = 0; j < n; j++) {
@@ -37,7 +36,6 @@ void settleAmounts(struct Person* persons, char** names, int n) {
     }
 
     while (1) {
-        // Find the maximum debtor and maximum creditor
         int maxDebit = 0, maxCredit = 0;
         int debtorIndex = -1, creditorIndex = -1;
 
@@ -51,26 +49,42 @@ void settleAmounts(struct Person* persons, char** names, int n) {
             }
         }
 
-        // If both a debtor and creditor are found, settle the amount
         if (debtorIndex != -1 && creditorIndex != -1) {
             int minAmount = (maxDebit < maxCredit) ? maxDebit : maxCredit;
 
             persons[debtorIndex].amount += minAmount;
             persons[creditorIndex].amount -= minAmount;
 
-            // Update net amounts
             netAmounts[debtorIndex] += minAmount;
             netAmounts[creditorIndex] -= minAmount;
 
-            // Print the transaction
             printf("%s owes %d to %s\n", names[debtorIndex], minAmount, names[creditorIndex]);
         } else {
-            // If no valid transaction can be performed, exit the loop
             break;
         }
     }
 
     free(netAmounts);
+}
+
+void addNewPerson(struct Person** persons, char*** names, int* n) {
+    (*n)++;
+    *names = realloc(*names, (*n) * sizeof(char*));
+    (*names)[*n - 1] = malloc(50 * sizeof(char));
+    printf("Enter the name of the new person: ");
+    scanf("%s", (*names)[*n - 1]);
+
+    *persons = realloc(*persons, (*n) * sizeof(struct Person));
+    strcpy((*persons)[*n - 1].name, (*names)[*n - 1]);
+    (*persons)[*n - 1].debts = malloc((*n) * sizeof(int));
+    (*persons)[*n - 1].amount = 0;
+
+    for (int i = 0; i < *n - 1; i++) {
+        printf("Enter the amount owed by %s to %s: ", (*names)[i], (*names)[*n - 1]);
+        scanf("%d", &((*persons)[i].debts[*n - 1]));
+        (*persons)[i].amount += (*persons)[i].debts[*n - 1];
+        (*persons)[*n - 1].debts[i] = 0;
+    }
 }
 
 int main() {
@@ -86,13 +100,27 @@ int main() {
     }
 
     struct Person* persons = malloc(n * sizeof(struct Person));
-
     initializePersons(persons, names, n);
 
     printf("\nSettlements:\n");
     settleAmounts(persons, names, n);
 
-    // Free allocated memory
+    printf("Would you like to add a new person? (y for yes and n for no)\n");
+    char c;
+    scanf(" %c", &c);
+
+    while (c == 'y') {
+        addNewPerson(&persons, &names, &n);
+        printf("\nUpdated Settlements:\n");
+        settleAmounts(persons, names, n);
+
+        printf("Would you like to add a new person? (y for yes and n for no)\n");
+        scanf(" %c", &c);
+        if (c=='n'){
+            break;
+        }
+    }
+
     for (int i = 0; i < n; i++) {
         free(names[i]);
         free(persons[i].debts);
